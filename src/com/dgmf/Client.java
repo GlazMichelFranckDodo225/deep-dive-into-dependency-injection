f1676d14f5c531a09046665609fb76ca5d2c9b5d
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-06-17 08:56:34 +0200

    Without Dependency Injection

---
+++ src/com/dgmf/Client.java
@@ -0,0 +1,15 @@
+package com.dgmf;
+
+// The Client Class Contains a Service Member Variable
+// Initialized in Its Constructor.
+public class Client {
+    private Service service;
+
+    public Client() {
+        System.out.println("Client Instantiation ...");
+
+        // The Client Directly Constructs and Controls which
+        // Service it Uses, Creating a Hard-Coded Dependency
+        this.service = new Service();
+    }
+}
---
+++ src/com/dgmf/Service.java
@@ -0,0 +1,7 @@
+package com.dgmf;
+
+public class Service {
+    public Service() {
+        System.out.println("I am the Required Service ...");
+    }
+}
